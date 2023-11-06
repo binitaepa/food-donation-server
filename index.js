0890b3473b30e69c4dbb04ab257b8e216456d128
binitaepa <binitaepa@gmail.com>
2023-11-07 04:18:47 +0600

    food details added

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
         const result=await cursor.toArray();
         res.send(result);
     })
+    app.get('/feature/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await cardCollection.findOne(query);
+        res.send(result);
+    })
 
     app.get('/collection', async (req, res) => {
         const cursor = foodCollection.find();
